b828d465a6fc03b6d84c4232b83b06d732e793cf
linwinpei <52321057+linwinpei@users.noreply.github.com>
2019-07-05 11:42:04 -0700

    Updates

---
+++ precache-manifest.9843d25322adfb0071c300c8330aaccc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35117c3e97b3f0f02325caca0fc1a7c9",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "319b46ae0ffd0b868c9f",
+    "url": "/movie_app/static/css/main.d71df24c.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/movie_app/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "319b46ae0ffd0b868c9f",
+    "url": "/movie_app/static/js/main.01f57b6c.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
